edd5d8a571e88e7b4dbdbcad97ac76929bd13b00
martin <martin.rougeron@epitech.eu>
2021-09-23 11:11:43 +0200

    babel.cpp init win

--- src/server/babel.cpp
+++ src/server/babel.cpp
@@ -5,6 +5,31 @@
 ** babel
 */
 
-int main(void)
-{
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QTextEdit>
+
+class Absolute : public QWidget {
+
+public:
+    Absolute(QWidget *parent = nullptr);
+};
+
+Absolute::Absolute(QWidget *parent)
+        : QWidget(parent) {
+
+    auto *ledit = new QTextEdit(this);
+    ledit->setGeometry(5, 5, 200, 150);
+}
+
+int main(int argc, char *argv[]) {
+
+    QApplication app(argc, argv);
+
+    Absolute window;
+
+    window.setWindowTitle("Absolute");
+    window.show();
+
+    return app.exec();
 }
